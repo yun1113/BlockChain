bbc6e60582799a4e43717793e35c2c43db62ad0b
yun1113 <yunyun1113c@gmail.com>
2017-04-16 14:36:45 +0800

    fix null array

--- src/WalletSerializer.java
+++ src/WalletSerializer.java
@@ -8,23 +8,25 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class WalletSerializer implements JsonSerializer<Wallet>{
+public class WalletSerializer implements JsonSerializer<Wallet> {
 
 	@Override
 	public JsonElement serialize(Wallet wallet, Type arg1, JsonSerializationContext arg2) {
 		JsonObject result = new JsonObject();
-        result.add("uuid", new JsonPrimitive(wallet.getUUID()));
-        result.add("address_num", new JsonPrimitive(wallet.getAddressNum()));
-        result.add("account", new JsonPrimitive(wallet.getAccount()));
-        
-        JsonArray jsonArray = new JsonArray();
-        ArrayList<String> address_list = wallet.getAddress();
-        for (String value : address_list) {
-            jsonArray.add(new JsonPrimitive(value));
-        }
-        result.add("address_list", jsonArray);
+		result.add("uuid", new JsonPrimitive(wallet.getUUID()));
+		result.add("address_num", new JsonPrimitive(wallet.getAddressNum()));
+		result.add("account", new JsonPrimitive(wallet.getAccount()));
 
-        return result;
+		JsonArray jsonArray = new JsonArray();
+		ArrayList<String> address_list = wallet.getAddress();
+		if (address_list != null) {
+			for (String value : address_list) {
+				jsonArray.add(new JsonPrimitive(value));
+			}
+			result.add("address_list", jsonArray);
+		}
+
+		return result;
 	}
 
 }
