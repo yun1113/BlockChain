05c74c5b1c8980264f950c021fae7ab14b0d3510
yun1113 <yunyun1113c@gmail.com>
2017-04-16 16:31:48 +0800

    transaction initial

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,5 +1,12 @@
 
 public class Transaction {
+	
+	private String version = null;
+	private int input_count = 0;
+	private int output_count = 0;
+	private String input_list = null;
+	private String output_list = null;
+	private String timestamp = null;
 
 	public Transaction() {
 		// TODO Auto-generated constructor stub
