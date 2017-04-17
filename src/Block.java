84cba0468c46415d8e948c6f573d58619f7237d9
yun1113 <yunyun1113c@gmail.com>
2017-04-17 09:17:45 +0800

    block validate

--- src/Block.java
+++ src/Block.java
@@ -1,22 +1,35 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 
+import org.bitcoinj.core.Sha256Hash;
+
 public class Block {
-	
-	private String block_id =null;
+
+	private String block_id = null;
 	private String version = null;
 	private String parent_block = null;
 	private String merkle_root = null;
-	private String difficulty = null;
+	private BigInteger difficulty = null;
 	private String timestamp = null;
-	private ArrayList<Transaction> transaction_list = new ArrayList<Transaction>(); 
-	
-	
+	private int nonce = 0;
+	private ArrayList<Transaction> transaction_list = new ArrayList<Transaction>();
+
 	public Block(String parent_block) {
 		this.parent_block = parent_block;
 	}
-	
-	private void blockValidate(){
-		
+
+	private void validateBlock() {
+		String hash_string = null;
+		hash_string = parent_block + merkle_root + timestamp;
+		int nonce = 0;
+		while (true) {
+			Sha256Hash hash = Sha256Hash.wrap(hash_string + nonce);
+			if (hash.toBigInteger().compareTo(difficulty) < 0) {
+				this.nonce = nonce;
+				break;
+			}
+			nonce += 1;
+		}
 	}
 
 }
